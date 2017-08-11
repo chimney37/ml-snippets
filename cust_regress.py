246bda8268b8cf6d48d0cb187487c5adb2681735
lxuser <chimney37@hotmail.com>
2017-08-11 16:01:37 +0900

    data generation added

--- cust_regress.py
+++ cust_regress.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from statistics import mean
 from matplotlib import style
 
@@ -28,17 +29,35 @@ def coefficient_of_determination(ys_orig,ys_line):
     squared_error_y_mean = squared_error(ys_orig,y_mean_line)
     return 1 - (squared_error_regr/squared_error_y_mean)
 
+#creating dataset
+def create_dataset(hm,variance, step=2,correlation=False):
+    val =1
+    ys=[]
+    for i in range(hm):
+        y = val + random.randrange(-variance,variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val-=step
+
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64),np.array(ys,dtype=np.float64)
+
+#create a data set
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
+
 m, b = best_fit_slope_and_intercept(xs,ys)
-print(m,b)
+print('best slope coeffcient=',m,'best intercept=',b)
 
-# plot a function based on the prediction of model
 regression_line = [(m*x)+b for x in xs]
-
+# plot a function based on the prediction of model
 r_squared = coefficient_of_determination(ys,regression_line)
-print(r_squared)
+print('given line variables, coeffcient of determination=',r_squared)
 
-#plt.scatter(xs,ys,color='#003F72')
-#plt.plot(xs, regression_line)
-#plt.show()
+plt.scatter(xs,ys,color='#003F72',label='data')
+plt.plot(xs, regression_line, label='regression line')
+plt.legend(loc=4)
+plt.show()
 
 
