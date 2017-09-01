db2e4ddc69de3908c1b8527f800752e22034f253
lxuser <chimney37@hotmail.com>
2017-09-01 17:04:15 +0900

    Added useful comments for better understanding of code

--- svm_cust.py
+++ svm_cust.py
@@ -63,76 +63,103 @@ class Support_Vector_Machine:
         all_data = None
 
         #create steps to solve the convex problem.
+        # We have 3 levels of steps, that progressively gets more fine.
         step_sizes =[self.max_feature_value * 0.1,
                      self.max_feature_value * 0.01,
                      # starts getting very high cost after this
                      self.max_feature_value * 0.001]
 
-        # Set b value modifiers: extremely expensive
+        # Set b value modifiers: extremely expensive, so we make b larger
+        # we can do this because the precision for b is not as important as w
         b_range_multiple = 2
         b_multiple = 5
         latest_optimum = self.max_feature_value*10
 
-        #begin stepping
+        #begin stepping with the lowest granularity, then progress to more finer
+        # step sizes
         for step in step_sizes:
+            # intialize w with values
             w = np.array([latest_optimum, latest_optimum])
             # we can do this because convex
             optimized = False
             while not optimized:
+                #np.arange function creates an array range given a start (negative
+                #value of multipled maximum feature value), stop(positive value) and step
                 for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
                                    self.max_feature_value*b_range_multiple,
                                    step*b_multiple):
+                    #transform w using each transform
                     for transformation in transforms:
                         w_t = w*transformation
                         found_option = True
-                        # weakest link in the SVM fundamentally
-                        # SMO attempts to fix this a bit
-                        # yi(xi.w+b) >= 1
 
+                        # check constraint requirement, yi(xi.w+b) >= 1
+                        # can add a break if any feature does not satisfy constraint..
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi=i
-                                # check against constraint requirement
                                 if not yi*(np.dot(w_t,xi)+b) >= 1:
                                     found_option = False
+                                    break
                                     #print(xi,':',yi*(np.dot(w_t,xi)+b))
 
+                        #if constraints is satisfied, register a key using matrix
+                        # norm (here it simply refers to ||w_t|| the magnitude) and the values as a possible w(transformed) and b
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+                            #print(opt_dict)
+
                 # once we pass 0, no reason to continue
+                # reason is that we find optimal values of the transforms (the
+                # negatives), so past 0 means we are complete in terms of
+                # optimization using a particular step
                 if w[0] < 0:
                     optimized = True
-                    print('Optimized a step')
+                    print('Optimized a step:' + str(step))
                 else:
                     w = w - step
 
+            #Once we get all candidates for a given step, we get the sorted keys which are the norm
+            #magnitude of w, or ||w||.  We want the value of w which corresponds to
+            #the smallest ||w||,so we use the smallest value of the norm,
+            #norms[0], and its corresponding b. Also the latest_optimum in the convex
+            #curve is updated to take a value x in the w vector. 
+            #comment: what is the purpose of +step*2?
             norms = sorted([n for n in opt_dict])
-            #||w|| : [w,b]
             opt_choice = opt_dict[norms[0]]
             self.w = opt_choice[0]
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0]+step*2
 
+        #see what the feature corresponds in terms of the constraint
+        #this print out is to illustrate that the training data met the constraint
+        #for support vectors
         for i in self.data:
             for xi in self.data[i]:
                 yi=i
                 print(xi,':',yi*(np.dot(self.w,xi)+self.b))
 
 
+    #prediction is simply sign(x.w + b) for SVM
+    #also visualizes the points for prediction
     def predict(self,features):
-        # sign(x.w+b)
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
         # if classification isn't 0 and we have visualization, we graph
         if classification != 0 and self.visualization:
-            self.ax.scatter(features[0],features[1],s=200,marker='*',
+            self.ax.scatter(features[0],features[1],s=100,marker='*',
                             c=self.colors[classification])
         else:
             print('featureset',features, 'is on the decision boundary')
         return classification
 
+    #draw a few things:
+    # 1. positive support vector hyperplane
+    # 2. negative support vector hyperplane
+    # 3. decision boundary hyperplane
+    # 4. featuresets data (training data) 
     def visualize(self):
         #scattering known featuresets
-        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in
+        [[self.ax.scatter(x[0],x[1],s=50,color=self.colors[i]) for x in
           data_dict[i]] for i in data_dict]
 
         def hyperplane(x,w,b,v):
