1f7c9cdc9fa587ce03a33dd976a2df7b661eca60
lxuser <chimney37@hotmail.com>
2017-11-26 20:51:39 +0900

    Fixed delimiters and other data checking functions

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,7 +16,7 @@ from collections import Counter
 import pickle
 import numpy as np
 import pandas as pd
-
+import re
 '''
 Notes on the dataset: Stanford's 1.6MM examples
 http://help.sentiment140.com/for-students/
@@ -30,7 +30,7 @@ tweet
 
 class SentimentPreProcess():
     def __init__(self):
-        self.data_delimiter=':;#$%&'
+        self.data_delimiter='\t'
         self.lemmatizer=WordNetLemmatizer()
 
     def init_process(self,fin,fout):
@@ -38,23 +38,24 @@ class SentimentPreProcess():
         with open(fin, buffering=200000, encoding='latin-1') as f:
             try:
                 for line in f:
-                    line = line.replace('"','')
-                    elements=line.split(',')
-                    initial_polarity = elements[0]
+                    elements=line.split('","')
+                    initial_polarity = elements[0].replace('"','')
                     if initial_polarity == '0':
+                        #negative: argmax=0
                         initial_polarity = [1,0]
                     elif initial_polarity == '4':
+                        #positive: argmax=1
                         initial_polarity = [0,1]
 
-                    tweet = elements[-1]
-                    outline = str(initial_polarity)+self.data_delimiter+tweet
+                    tweet = elements[-1].replace('"','')
+                    outline =str(initial_polarity)+self.data_delimiter+tweet
                     outfile.write(outline)
             except Exception as e:
                 print(str(e))
         outfile.close()
 
     def create_lexicon(self,fin):
-        lexicon=[]
+        self.lexicon=[]
         with open(fin,'r',buffering=100000, encoding='latin-1') as f:
             try:
                 counter=1
@@ -67,35 +68,46 @@ class SentimentPreProcess():
                         content+=' '+tweet
                         words = word_tokenize(content)
                         words = [self.lemmatizer.lemmatize(i).lower() for i in words]
-                        lexicon=list(set(lexicon+words))
-                        print(counter,len(lexicon))
-
+                        self.lexicon=list(set(self.lexicon+words))
+                        print(counter,len(self.lexicon))
             except Exception as e:
                 print(str(e))
 
         with open('lexicon.pickle','wb') as f:
-            pickle.dump(lexicon,f)
+            pickle.dump(self.lexicon,f)
+
+    def load_lexicon(self,lexicon_pickle):
+        with open(lexicon_pickle,'rb') as f:
+            self.lexicon = pickle.load(f)
+
+    def extract_features(self,input_data,lexicon):
+        current_words = word_tokenize(input_data.lower())
+        current_words = [self.lemmatizer.lemmatize(i).lower() for i in current_words]
+        features = np.zeros(len(lexicon))
+
+        for word in current_words:
+            if word in lexicon:
+                index_value = lexicon.index(word)
+                features[index_value]+=1
+
+        return features
 
     def convert_to_vec(self,fin,fout,lexicon_pickle):
-        with open(lexicon_pickle,'rb') as f:
-            lexicon = pickle.load(f)
+        self.load_lexicon(lexicon_pickle)
         outfile = open(fout,'w')
         with open(fin,buffering=20000,encoding='latin-1') as f:
             counter=0
+            pattern="^\[.*\]"
             for line in f:
                 counter+=1
                 label=line.split(self.data_delimiter)[0]
                 tweet=line.split(self.data_delimiter)[1]
-                current_words=word_tokenize(tweet.lower())
-                current_words=[self.lemmatizer.lemmatize(i) for i in current_words]
 
-                features=np.zeros(len(lexicon))
+                #skip neutral data(only use positive or negative data)
+                if(re.match(pattern,label) is None):
+                    continue
 
-                for word in current_words:
-                    if word.lower() in lexicon:
-                        index_value=lexicon.index(word.lower())
-                        features[index_value]+=1
-
+                features=self.extract_features(tweet,self.lexicon)
                 features=list(features)
                 outline=str(features)+self.data_delimiter+str(label)+'\n'
                 outfile.write(outline)
@@ -103,11 +115,12 @@ class SentimentPreProcess():
             print("Size of test data:",str(counter))
 
     def shuffle_data(self,fin):
-        df=pd.read_csv(fin,names=['sentiment','tweet'],error_bad_lines=False)
+        df=pd.read_csv(fin,names=['sentiment','tweet'],sep=self.data_delimiter,error_bad_lines=False,warn_bad_lines=False)
+        print(Counter(df['sentiment']))
         df=df.iloc[np.random.permutation(len(df))]
         print(df.head())
-        print(Counter(df['sentiment']))
-        df.to_csv('train_set_shuffled.csv',header=False,index=False)
+        df.to_csv('train_set_shuffled.csv',sep=self.data_delimiter,header=False,index=False)
+
 
     def create_test_data_pickle(self,fin):
         feature_sets=[]
@@ -136,7 +149,6 @@ class SentimentPreProcess():
         #create training and test set
         self.init_process('training.1600000.processed.noemoticon.csv','train_set.csv')
         self.init_process('testdata.manual.2009.06.14.csv','test_set.csv')
-
         self.create_lexicon('train_set.csv')
 
         #create a pre-processed vectorized set for testing
