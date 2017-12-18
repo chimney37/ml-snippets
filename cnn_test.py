e535d3a0d853f751643f3a5afed482cf5bd16ada
lxuser <chimney37@hotmail.com>
2017-12-18 20:18:25 +0900

    fixed some syntax issues using flake8

--- cnn_test.py
+++ cnn_test.py
@@ -9,6 +9,9 @@
     Date created: 12/09/2017
     Python Version: 3.62
 '''
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
 '''
 We will use the MNIST data, using 60000 training samples and 10000 testing samples of
 handwritten and labeled digits, 0 through 9, i.e. 10 total "classes". In actual deep
@@ -19,10 +22,10 @@ any computers. MNIST dataset of images 28x28:784 pixels. Either pixel is "blank"
 
 In a traditional neural network, input data will send to hidden layer 1, that is weighted. It will undergo an
 activation function, so neuron can decided to fire and output data to either output
-layer, or another hidden layer. 
+layer, or another hidden layer.
 
 In a Convolutional Neural Network (CNN), the processes go by: Convolution =>
-Pooling => Convolution => Pooling => Fully connected layer => Output.Each term
+Pooling => Convolution => Pooling => Fully connected layer => Output. Each term
 between the arrows ia a layer. Convolution is the act of taking original data, and creating feature maps from
 it. Pooling is down-sampling, most often in the form of "max-pooling", where we
 select a region, and then take the maximum value in that region, and that
@@ -55,23 +58,19 @@ We will use a cost function (loss function), to determine how wrong we are. We
 will use an optimizer function: Adam optimizer, to minimize the cost. Cost is
 minimized by tinkering with weights. How quickly we want to lower the cost is determined by
 learning rate. The lower the value for learning rate, the slower we will learn, and
-more likely we'd get better results. 
+more likely we'd get better results.
 
 One training cycle over the entire piece of data is called an Epoch. We can pick any number for number of epochs. After each epoch, we've
 hopefully further fine-tuned our weights lowering our cost and improving accuracy.
-
 '''
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
 # one_hot means one eleent out of others is literally "hot" or on. This is useful for a
 # multi-class classification, from 0,1,...to 9. So we want the output to be like
 #
 # 0 = [1,0,0,0,0,0,0,0,0]
 # 1 = [0,1,0,0,0,0,0,0,0]
-#...
+# ...
 # 9 = [0,0,0,0,0,0,0,0,1]
-mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 '''
 in building the model, we consider the number of nodes each hidden layer will have.
@@ -80,30 +79,32 @@ we are trying to model (TBD).
 
 Batches are used to control how many features we are going to optimize at once, as computers
 are limited by memory.
-
 '''
 n_classes = 10
 batch_size = 128
-#number of cycles of feedforward and backward propagation
+# number of cycles of feedforward and backward propagation
 hm_epochs = 9
- 
-#input: 784 is pixels. the matrix is 1 x 2 because we flatten the image: 28x28 to a
-#784 values. This is also known as the shape. If an input data is out of place that doen't
-#fit the shape, this specification will ignore the data, without throwing an error.
-x = tf.placeholder('float', [None,784])
-#output
+
+# input: 784 is pixels. The matrix is 1 x 2 because we flatten the image: 28x28 to a
+# 784 values. This is also known as the shape. If an input data is out of place that doen't
+# fit the shape, this specification will ignore the data, without throwing an error.
+x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
 
-# Functions here are the same from official tensorflow CNN tutorial. 
+# Functions here are the same from official tensorflow CNN tutorial.
 # strides indicate movement of window. 1 means we just move 1 and a time, in
 # conv2d, and 2 at a time in maxpool2d. ksize is size of pooling window. In
 # this case we are using 2x2 pixels for pooling.
 # padding is SAME means the output image has same size as input
+
+
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def maxpool2d(x):
-    return tf.nn.max_pool(x,ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
 def convolutional_neural_network(x):
     # weights defined are tensorflow variables, we specify the shape of the variable
@@ -113,28 +114,28 @@ def convolutional_neural_network(x):
     # tf.random_normal outputs random values for the shape we want
 
     weights = {
-        # 5x5 convolution, 1 input image, 32 outputs 
-        'W_conv1': tf.Variable(tf.random_normal([5,5,1,32])),
+        # 5x5 convolution, 1 input image, 32 outputs
+        'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
         # 5x5 conv, 32 inputs, 64 outputs
-        'W_conv2': tf.Variable(tf.random_normal([5,5,32,64])),
+        'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
         # fully connected, 7x7x64 inputs, 1024 outputs
-        'W_fc':tf.Variable(tf.random_normal([7*7*64,1024])),
+        'W_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
         # 1024 inputs, 10 outputs (class prediction)
-        'out': tf.Variable(tf.random_normal([1024,n_classes]))
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
     }
 
     biases = {
-        'b_conv1':tf.Variable(tf.random_normal([32])),
-        'b_conv2':tf.Variable(tf.random_normal([64])),
-        'b_fc':tf.Variable(tf.random_normal([1024])),
-        'out':tf.Variable(tf.random_normal([n_classes]))
+        'b_conv1': tf.Variable(tf.random_normal([32])),
+        'b_conv2': tf.Variable(tf.random_normal([64])),
+        'b_fc': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
     }
- 
+
     # Reshape input to 4D tensor
-    x = tf.reshape(x,shape=[-1,28,28,1])
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
     # Convolution layer, using our function
     # relu: recified linear: this is the activation function (threshold)
-    conv1 = tf.nn.relu(conv2d(x,weights['W_conv1'])+biases['b_conv1'])
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1'])+biases['b_conv1'])
     # Max pooling (down-sampling)
     conv1 = maxpool2d(conv1)
     # Convolution layer
@@ -142,15 +143,16 @@ def convolutional_neural_network(x):
     # Max pooling (down-sampling)
     conv2 = maxpool2d(conv2)
 
-    #Fully connected Layer
-    #Reshape conv2 output to fit fully connected layer
-    fc = tf.reshape(conv2, [-1,7*7*64])
-    fc = tf.nn.relu(tf.matmul(fc,weights['W_fc'])+biases['b_fc'])
+    # Fully connected Layer
+    # Reshape conv2 output to fit fully connected layer
+    fc = tf.reshape(conv2, [-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc'])+biases['b_fc'])
 
-    #output layer has no activation
-    output = tf.matmul(fc,weights['out']) +biases['out']
+    # output layer has no activation
+    output = tf.matmul(fc, weights['out'])+biases['out']
 
     return output
+
 
 def train_neural_network(x):
     # we produce a prediction based on the neural network model
@@ -158,30 +160,31 @@ def train_neural_network(x):
     # cost function : loss function to optimize the cost. We will
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,
                                                                   labels=y))
-    #Adam optimizer is a optimizer along with others like SGD (stochastic graient
-    #descent and AdaGrad
-    #Adam optimizer can specifiy learning_rate, default is 0.001
+    # Adam optimizer is an optimizer along with others like SGD (stochastic gradient)
+    # descent and AdaGrad
+    # Adam optimizer can specifiy learning_rate, default is 0.001
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        for epoch in range (hm_epochs):
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x:epoch_x, y: epoch_y})
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
 
             print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
 
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y,1))
-        #tell us how many predictions we made that were perfect matches to their labels
-        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        # tell us how many predictions we made that were perfect matches to their labels
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
-#10 and 20 epochs should give ~95% accuracy, but 95% is actually nothing
+# 10 and 20 epochs should give ~95% accuracy, but 95% is actually nothing
 # great consider other methods. It's amazing that using just pixels can
 # actually achieve 95%. state of art is 99%
+
 
 train_neural_network(x)
--- lstm_rnn.py
+++ lstm_rnn.py
@@ -9,6 +9,9 @@
     Date created: 12/09/2017
     Python Version: 3.62
 '''
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.ops import rnn, rnn_cell
 '''
 We will use the MNIST data, using 60000 training samples and 10000 testing samples of
 handwritten and labeled digits, 0 through 9, i.e. 10 total "classes". Actual deep
@@ -42,18 +45,14 @@ more likely we'd get better results.
 One training cycle involving all data is called an Epoch. We can pick any number for number of epochs. After each epoch, we've
 hopefully further fine-tuned our weights lowering our cost and improving accuracy.
 '''
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.python.ops import rnn, rnn_cell
-
 # one_hot means one eleent out of others is literally "hot" or on. This is useful for a
 # multi-class classification, from 0,1,...to 9. So we want the output to be like
 #
 # 0 = [1,0,0,0,0,0,0,0,0]
 # 1 = [0,1,0,0,0,0,0,0,0]
-#...
+# ...
 # 9 = [0,0,0,0,0,0,0,0,1]
-mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 '''
 We are establishing the shape of the input (x) to include chunks. In a basic
@@ -69,12 +68,12 @@ chunk_size = 28
 n_chunks = 28
 rnn_size = 128
 
-#input: the image: 28x28 to a 28 chunks each consisting of a size of 28.
-#If an input data is out of place that doen't
-#fit the shape, this specification will ignore the data, without throwing an error.
-x = tf.placeholder('float', [None,n_chunks,chunk_size])
-#output
+# input: the image: 28x28 to a 28 chunks each consisting of a size of 28.
+# If an input data is out of place that doen't
+# fit the shape, this specification will ignore the data, without throwing an error.
+x = tf.placeholder('float', [None, n_chunks, chunk_size])
 y = tf.placeholder('float')
+
 
 def recurrent_neural_network(x):
     # Defining the layers and the output
@@ -83,23 +82,24 @@ def recurrent_neural_network(x):
     # (input*weight)+biases. Biases will make sure a neuron may still fire if
     # all inputs are 0.
     # rnn_size:
-    layer = {'weights': tf.Variable(tf.random_normal([rnn_size,n_classes])),
+    layer = {'weights': tf.Variable(tf.random_normal([rnn_size, n_classes])),
              'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     # checknumpy's tranpose for details. Basically changes the shape (from e.g.
-    # (1,2,3) to (2,1,3). debug print to figure out 
-    x = tf.transpose(x, [1,0,2])
+    # (1,2,3) to (2,1,3). debug print to figure out
+    x = tf.transpose(x, [1, 0, 2])
     x = tf.reshape(x, [-1, chunk_size])
     x = tf.split(x, n_chunks, 0)
 
-    #rnn_cell is defined by tensorflow
-    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size,state_is_tuple=True)
+    # rnn_cell is defined by tensorflow
+    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size, state_is_tuple=True)
     outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
-    #output layer has no activation
-    output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
+    # output layer has no activation
+    output = tf.matmul(outputs[-1], layer['weights'])+layer['biases']
 
     return output
+
 
 def train_neural_network(x):
     # we produce a prediction based on the neural network model
@@ -107,30 +107,30 @@ def train_neural_network(x):
     # cost function : loss function to optimize the cost. We will
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,
                                                                   labels=y))
-    #Adam optimizer is a optimizer along with others like SGD (stochastic graient
-    #descent and AdaGrad
-    #Adam optimizer can specifiy learning_rate, default is 0.001
+    # Adam optimizer is a optimizer along with others like SGD (stochastic graient
+    # descent and AdaGrad
+    # Adam optimizer can specifiy learning_rate, default is 0.001
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    #number of cycles of feedforward and backward propagation
+    # number of cycles of feedforward and backward propagation
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        for epoch in range (hm_epochs):
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                epoch_x = epoch_x.reshape((batch_size,n_chunks,chunk_size))
-                _, c = sess.run([optimizer, cost], feed_dict={x:epoch_x, y: epoch_y})
+                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
 
             print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
 
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y,1))
-        #tell us how many predictions we made that were perfect matches to their labels
-        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        # tell us how many predictions we made that were perfect matches to their labels
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
         print('Accuracy:',
-              accuracy.eval({x:mnist.test.images.reshape((-1,n_chunks,chunk_size)), y:mnist.test.labels}))
+              accuracy.eval({x: mnist.test.images.reshape((-1, n_chunks, chunk_size)), y: mnist.test.labels}))
 
-#train neural network
+
 train_neural_network(x)
