a2cd211a6d3e72d14439fddebb9272f43044194e
lxuser <chimney37@hotmail.com>
2017-12-19 22:06:00 +0900

    cleaned up code against PEP8

--- titanic_data.py
+++ titanic_data.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding:utf-8
-# Handling non-numerical data (titanic.xls) and KMeans to predict survivability 
+# Handling non-numerical data (titanic.xls) and KMeans to predict survivability
 # Special thanks: Harisson@pythonprogramming.net
 
 '''
@@ -9,14 +9,10 @@
     Date created: 09/15/2017
     Python Version: 3.62
 '''
-import matplotlib.pyplot as plt
-from matplotlib import style
 import numpy as np
-import cust_kmeans as ckm 
-from sklearn.cluster import KMeans
-from sklearn import preprocessing, cross_validation
+from sklearn import preprocessing
+import cust_kmeans as ckm
 import pandas as pd
-style.use('ggplot')
 
 
 '''
@@ -35,66 +31,68 @@ boat Lifeboat
 body Body Identification Number
 home.dest Home/Destination
 '''
-#read and print raw contents
+# read and print raw contents
 df = pd.read_excel('titanic.xls')
 print(df.head())
-df.drop(['body','name'],1,inplace=True)
+df.drop(['body', 'name'], 1, inplace=True)
 df.convert_objects(convert_numeric=True)
 df.fillna(0, inplace=True)
 print(df.head())
 
+
 def handle_non_numerical_data(df):
-    columns= df.columns.values
+    columns = df.columns.values
 
     for column in columns:
         text_digit_vals = {}
 
-        #memozation of data using a dict
+        # memozation of data using a dict
         def convert_to_int(val):
             return text_digit_vals[val]
 
-        #check data type of each column
+        # check data type of each column
         if df[column].dtype != np.int64 and df[column].dtype != np.float64:
             column_contents = df[column].values.tolist()
-            #get a set of unique elements from each column
+            # get a set of unique elements from each column
             unique_elements = set(column_contents)
-            x=0
-            #assign an index for each unique element not already tracked in dictionary
+            x = 0
+            # assign an index for each unique element not already tracked in dictionary
             for unique in unique_elements:
                 if unique not in text_digit_vals:
-                    text_digit_vals[unique]=x
-                    x+=1
+                    text_digit_vals[unique] = x
+                    x += 1
 
-            df[column] = list(map(convert_to_int,df[column]))
+            df[column] = list(map(convert_to_int, df[column]))
 
     return df
+
 
 # do non-numerical processing and assign numericals to non-numerical values
 df = handle_non_numerical_data(df)
 print(df.head())
 
-#drop features to see impact
-df.drop(['ticket','home.dest'],1,inplace=True)
+# drop features to see impact
+df.drop(['ticket', 'home.dest'], 1, inplace=True)
 
-#training data, take out the survived since that is what we want predicted
-X=np.array(df.drop(['survived'],1).astype(float))
-X=preprocessing.scale(X)
-y=np.array(df['survived'])
+# training data, take out the survived since that is what we want predicted
+X = np.array(df.drop(['survived'], 1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
 
 print(y)
 
-#use the custom kmeans
+# use the custom kmeans
 clf = ckm.K_Means()
-#clf = KMeans(n_clusters=2)
+# clf = KMeans(n_clusters=2)
 clf.fit(X)
 
 # a bit strange as the predictio can be 0 or 1 (switched?)
 correct = 0
 for i in range(len(X)):
-    predict_me=np.array(X[i].astype(float))
-    predict_me=predict_me.reshape(-1,len(predict_me))
-    prediction=clf.predict(predict_me)
-    if prediction==y[i]:
-        correct+=1
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = clf.predict(predict_me)
+    if prediction == y[i]:
+        correct += 1
 
 print(correct/len(X))
