1c8933790f7580c8a8ca11e87ae953d6cfa2c59f
lxuser <chimney37@hotmail.com>
2017-12-21 22:15:24 +0900

    fixing a file per PEP8

--- knn.py
+++ knn.py
@@ -12,7 +12,7 @@
 '''
 import sys
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
+from sklearn import cross_validation, neighbors
 import pandas as pd
 
 # Column attributes (id,clump_thickness,uniform_cell_size,
@@ -22,19 +22,19 @@ import pandas as pd
 # For more info, see Attribute information:
 # https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
 df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?',-sys.maxsize, inplace=True)
+df.replace('?', -sys.maxsize, inplace=True)
 df.drop(['id'], 1, inplace=True)
 
 # drop the column(s) not used for training
-X = np.array(df.drop(['class'],1))
+X = np.array(df.drop(['class'], 1))
 
 # labels (for prediction)
 Y = np.array(df['class'])
 
-#create training and testing samples
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y,test_size=0.2)
+# create training and testing samples
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.2)
 
-#Train the classifier
+# Train the classifier
 clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train, y_train)
 
@@ -42,8 +42,8 @@ accuracy = clf.score(X_test, y_test)
 print('accuracy=', accuracy)
 
 # crete example data for prediction
-example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)
+example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 1, 1, 2, 3, 2, 1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
 
 prediction = clf.predict(example_measures)
 print(prediction)
